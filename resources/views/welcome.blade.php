4b8d0c72a9b2a83ecbc87b83c18fe13889639438
Garraro <eliassyo@gmail.com>
2019-02-21 00:47:40 -0300

    Sweet Alert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
